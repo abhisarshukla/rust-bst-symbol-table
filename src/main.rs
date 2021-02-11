42bcdcd8a98d8cb114b5b966e6783b6de75e1231
abhisarshukla <abhisarshukla645@gmail.com>
2021-02-11 18:01:11 +0530

    Add min, max, floor and ceiling implementations

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ where
 
     fn put(&mut self, key: K, value: V) {
         match &self.key {
-            Option::Some(k) => match key.cmp(&k) {
+            Option::Some(k) => match key.cmp(k) {
                 Ordering::Less => match &mut self.left {
                     Option::Some(node) => node.put(key, value),
                     Option::None => {
@@ -90,6 +90,84 @@ where
         }
         self.size = Self::size_node(&self.left) + Self::size_node(&self.right) + 1;
     }
+
+    fn min(&self) -> Option<&V> {
+        match &self.left {
+            Option::Some(node) => node.min(),
+            Option::None => match &self.value {
+                Option::Some(v) => Option::Some(v),
+                Option::None => Option::None,
+            },
+        }
+    }
+
+    fn max(&self) -> Option<&V> {
+        match &self.right {
+            Option::Some(node) => node.max(),
+            Option::None => match &self.value {
+                Option::Some(v) => Option::Some(v),
+                Option::None => Option::None,
+            },
+        }
+    }
+
+    fn floor(&self, key: K) -> Option<&V> {
+        match &self.key {
+            Option::Some(k) => match key.cmp(k) {
+                Ordering::Less => match &self.left {
+                    Option::Some(node) => node.floor(key),
+                    Option::None => Option::None,
+                },
+                Ordering::Equal => match &self.value {
+                    Option::Some(v) => Option::Some(v),
+                    Option::None => Option::None,
+                },
+                Ordering::Greater => match &self.right {
+                    Option::Some(node) => match node.floor(key) {
+                        Option::Some(v) => Option::Some(v),
+                        Option::None => match &self.value {
+                            Option::Some(v) => Option::Some(v),
+                            Option::None => Option::None,
+                        },
+                    },
+                    Option::None => match &self.value {
+                        Option::Some(v) => Option::Some(v),
+                        Option::None => Option::None,
+                    },
+                },
+            },
+            Option::None => Option::None,
+        }
+    }
+
+    fn ceiling(&self, key: K) -> Option<&V> {
+        match &self.key {
+            Option::Some(k) => match key.cmp(k) {
+                Ordering::Greater => match &self.right {
+                    Option::Some(node) => node.ceiling(key),
+                    Option::None => Option::None,
+                },
+                Ordering::Equal => match &self.value {
+                    Option::Some(v) => Option::Some(v),
+                    Option::None => Option::None,
+                },
+                Ordering::Less => match &self.left {
+                    Option::Some(node) => match node.ceiling(key) {
+                        Option::Some(v) => Option::Some(v),
+                        Option::None => match &self.value {
+                            Option::Some(v) => Option::Some(v),
+                            Option::None => Option::None,
+                        },
+                    },
+                    Option::None => match &self.value {
+                        Option::Some(v) => Option::Some(v),
+                        Option::None => Option::None,
+                    },
+                },
+            },
+            Option::None => Option::None,
+        }
+    }
 }
 
 fn main() {
@@ -98,7 +176,9 @@ fn main() {
     st.put(11, String::from("Is"));
     st.put(17, String::from("A"));
     st.put(12, String::from("Test"));
-    st.put(15, String::from("That"));
+    st.put(16, String::from("That"));
+    st.put(19, String::from("Not"));
+
     println!("{:#?}", st);
 
     if let Option::Some(val) = st.get(12) {
@@ -107,4 +187,20 @@ fn main() {
         println!("Key not found");
     }
     println!("{}", st.size());
+    if let Option::Some(val) = st.min() {
+        println!("Value associated with min key is {:?}", &val);
+    }
+    if let Option::Some(val) = st.max() {
+        println!("Value associated with max key is {:?}", &val);
+    }
+    if let Option::Some(val) = st.floor(16) {
+        println!("floor = {:?}", &val);
+    } else {
+        println!("floor value could not be found!");
+    }
+    if let Option::Some(val) = st.ceiling(10) {
+        println!("ceiling = {:?}", &val);
+    } else {
+        println!("ceiling value could not be found!");
+    }
 }
